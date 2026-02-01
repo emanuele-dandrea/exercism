816f1993be81a3e8ba101cccf4c4ac3262db2ce5
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-02-01 14:15:31 +0100

    Python: Grains

---
+++ python/grains/grains.py
@@ -0,0 +1,10 @@
+def square(number):
+    if number < 1 or number > 64:
+        raise ValueError("square must be between 1 and 64")
+    return 2 ** (number - 1)
+
+def total():
+    totalGrains = 0
+    for number in range (1, 65):
+        totalGrains += square(number)
+    return totalGrains
